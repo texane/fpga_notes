3524f8ea9f3c3581d0d2affbdb0100ed30e144ec
texane <texane@gmail.com>
2015-10-17 15:41:25 +0200

    add related

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # create version.tex
 echo -n '\\newcommand{\\version}{' > version.tex
-git rev-parse HEAD >> version.tex
+git rev-parse --short HEAD >> version.tex
 echo -n '}' >> version.tex
 
 texi2pdf main.tex
