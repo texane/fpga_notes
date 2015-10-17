d888b4606ee8a0b9c293d21ac0a03533f8149ab2
texane <texane@gmail.com>
2015-10-17 14:01:38 +0200

    add version

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
+# create version.tex
+echo -n '\\newcommand{\\version}{' > version.tex
+git rev-parse HEAD >> version.tex
+echo -n '}' >> version.tex
+
 texi2pdf main.tex
--- clean.sh
+++ clean.sh
@@ -3,3 +3,7 @@
 for f in main.{aux,bbl,blg,log,toc}; do
  [ -e $f ] && rm $f;
 done
+
+for f in version.{tex,tex.aux}; do
+ [ -e $f ] && rm $f;
+done
