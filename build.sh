c9e0cb3058402fe59b6b08e13b59386e6955e753
texane <texane@gmail.com>
2015-10-18 13:33:23 +0200

    version none

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # create version.tex
 echo -n '\\newcommand{\\version}{' > version.tex
-git rev-parse --short HEAD >> version.tex
+(git rev-parse --short HEAD || echo none) >> version.tex
 echo -n '}' >> version.tex
 
 texi2pdf main.tex
